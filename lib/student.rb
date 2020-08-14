5be4f9a803c6dd58f7d21d8b555a86db81c76f6f
Philip Warren <philwarren1@gmail.com>
2020-08-14 17:18:50 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,12 +1,12 @@
 class Student 
 
 def hello
-  "Hey there! I'm so excited to learn stuff."
-end
+    puts "Hey there! I'm so excited to learn stuff."
+  end
 
-def raise_hand
-"Pick me!"
-end
+  def raise_hand
+    puts "Pick me!"
+  end
 
 
 end
